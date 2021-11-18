ebd0cc9cb15895d76fab4ad7f047dfa2a0ef28cf
Soham Mitra <75801297+coderboy53@users.noreply.github.com>
2021-11-18 22:01:15 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<nav class = 'navBar'>
+       <div class ='title'>
+         <img src = './images/logo.png'>
+         <h1 class = 'sitehead'>Carbnb</h1>
+       </div>
+       <div class = 'navigationMenu'>
+         <a href = './index.php'>Home</a>
+         <a href = './about.php'>About</a>
+         <a href = './rent.php'>Rent a Car</a>
+         <a href = './regist_car.php'>Register your Car</a>
+         <a href = './safety.php'>Safety</a>
+         <a href = './contact_us.php'>Contact Us</a>
+         <a href ='./login.php' id = "signup">Sign In</a>
+       </div>
+     </nav>
