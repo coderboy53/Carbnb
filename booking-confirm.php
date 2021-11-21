28187d0d96a38b6298ebea7b0386fc58a05b790a
Soham Mitra <mitra.soham53@gmail.com>
2021-11-21 21:56:33 +0530

    removed error from rent_style, added 2 new files

---
+++ booking-confirm.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carbnb - Car Rentals</title>
+    <link href="booking_confirm_style.css" rel="stylesheet"/>
+</head>
+<body>
+    <main class="main">
+    <?php include('assets/navbar.php') ?>
+    <div class="center">
+        <h1>Confirm your booking details</h1>
+
+    </div>
+    </main>
+</body>
+</html>
