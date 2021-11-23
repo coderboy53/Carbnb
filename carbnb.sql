7f886696f69fb9ac145a7ae7216daa6fc5089a5a
Soham Mitra <mitra.soham53@gmail.com>
2021-11-23 12:58:50 +0530

    updated DB

--- carbnb.sql
+++ carbnb.sql
@@ -2,8 +2,8 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 22, 2021 at 05:33 PM
+-- Host: localhost
+-- Generation Time: Nov 23, 2021 at 08:24 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `ID` int(4) NOT NULL,
-  `PASSWORD` varchar(15) NOT NULL,
+  `PASSWORD` varchar(255) NOT NULL,
   `NAME` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -45,7 +45,7 @@ CREATE TABLE `booking` (
   `AMOUNT` int(11) NOT NULL,
   `DURATION` int(11) NOT NULL,
   `STATUS` varchar(1) NOT NULL,
-  `R_EMAIL` varchar(50) DEFAULT NULL,
+  `R_ID` int(11) NOT NULL,
   `C_VEHICLENO` varchar(13) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -60,8 +60,9 @@ CREATE TABLE `car` (
   `COMPANY` varchar(15) NOT NULL,
   `MODEL` varchar(30) NOT NULL,
   `AGE` int(11) NOT NULL,
+  `LOCATION` varchar(30) NOT NULL,
   `NO_OF_SEATS` int(11) NOT NULL,
-  `O_EMAIL` varchar(50) DEFAULT NULL
+  `O_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -74,7 +75,7 @@ CREATE TABLE `car_review` (
   `REVIEW_ID` int(11) NOT NULL,
   `DESCRIPTION` varchar(800) NOT NULL,
   `VEHICLENO` varchar(13) DEFAULT NULL,
-  `R_EMAIL` varchar(50) DEFAULT NULL,
+  `O_ID` int(11) NOT NULL,
   `SCORE` int(11) NOT NULL CHECK (`SCORE` <= 10)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -87,7 +88,7 @@ CREATE TABLE `car_review` (
 CREATE TABLE `feedback` (
   `FEEDBACK_ID` int(11) NOT NULL,
   `NAME` varchar(30) NOT NULL,
-  `EMAIL` varchar(50) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
   `PHONENUMBER` bigint(10) NOT NULL,
   `LOCATION` varchar(30) NOT NULL,
   `REVIEW_TYPE` varchar(10) NOT NULL,
@@ -103,10 +104,11 @@ CREATE TABLE `feedback` (
 --
 
 CREATE TABLE `owner` (
-  `EMAIL` varchar(50) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
   `NAME` varchar(30) NOT NULL,
   `PHONE_NO` bigint(10) NOT NULL,
-  `PASSWORD` varchar(15) NOT NULL
+  `PASSWORD` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -119,7 +121,7 @@ CREATE TABLE `payments` (
   `PAYMENT_ID` int(11) NOT NULL,
   `AMOUNT` int(11) NOT NULL,
   `PAYMENT_DATE` datetime NOT NULL,
-  `R_EMAIL` varchar(50) DEFAULT NULL,
+  `R_ID` int(11) NOT NULL,
   `A_ID` int(4) DEFAULT NULL,
   `BOOKING_ID` varchar(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -131,11 +133,12 @@ CREATE TABLE `payments` (
 --
 
 CREATE TABLE `renter` (
-  `EMAIL` varchar(50) NOT NULL,
-  `NAME` varchar(30) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `NAME` varchar(50) NOT NULL,
   `PHONE_NO` bigint(10) NOT NULL,
   `DRIVING_LICENSE_NO` varchar(15) NOT NULL,
-  `PASSWORD` varchar(15) NOT NULL
+  `PASSWORD` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -152,19 +155,22 @@ ALTER TABLE `admin`
 -- Indexes for table `booking`
 --
 ALTER TABLE `booking`
-  ADD PRIMARY KEY (`BOOKING_ID`);
+  ADD PRIMARY KEY (`BOOKING_ID`),
+  ADD KEY `R_ID` (`R_ID`);
 
 --
 -- Indexes for table `car`
 --
 ALTER TABLE `car`
-  ADD PRIMARY KEY (`VEHICLE_NO`);
+  ADD PRIMARY KEY (`VEHICLE_NO`),
+  ADD KEY `O_ID` (`O_ID`);
 
 --
 -- Indexes for table `car_review`
 --
 ALTER TABLE `car_review`
-  ADD PRIMARY KEY (`REVIEW_ID`);
+  ADD PRIMARY KEY (`REVIEW_ID`),
+  ADD KEY `O_ID` (`O_ID`);
 
 --
 -- Indexes for table `feedback`
@@ -176,19 +182,20 @@ ALTER TABLE `feedback`
 -- Indexes for table `owner`
 --
 ALTER TABLE `owner`
-  ADD PRIMARY KEY (`EMAIL`);
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `payments`
 --
 ALTER TABLE `payments`
-  ADD PRIMARY KEY (`PAYMENT_ID`);
+  ADD PRIMARY KEY (`PAYMENT_ID`),
+  ADD KEY `R_ID` (`R_ID`);
 
 --
 -- Indexes for table `renter`
 --
 ALTER TABLE `renter`
-  ADD PRIMARY KEY (`EMAIL`);
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -216,13 +223,53 @@ ALTER TABLE `car_review`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `FEEDBACK_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `FEEDBACK_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `owner`
+--
+ALTER TABLE `owner`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
   MODIFY `PAYMENT_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `renter`
+--
+ALTER TABLE `renter`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`R_ID`) REFERENCES `renter` (`ID`);
+
+--
+-- Constraints for table `car`
+--
+ALTER TABLE `car`
+  ADD CONSTRAINT `car_ibfk_1` FOREIGN KEY (`O_ID`) REFERENCES `owner` (`ID`);
+
+--
+-- Constraints for table `car_review`
+--
+ALTER TABLE `car_review`
+  ADD CONSTRAINT `car_review_ibfk_1` FOREIGN KEY (`O_ID`) REFERENCES `owner` (`ID`);
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`R_ID`) REFERENCES `renter` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
