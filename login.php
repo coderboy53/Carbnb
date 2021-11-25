604a65e12147520967b575c44860eb493839c7b5
Susindhar Arumugapriya Venkatesan <79799198+susindhar21@users.noreply.github.com>
2021-11-25 12:09:11 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,47 +1,62 @@
+<?php
+  session_start();
+  require_once "pdo.php";
+  $msg="";
+  if(isset($_POST['email']) && isset($_POST['psw']))
+  {
+    $psw= hash('md5',$_POST['psw']);
+    echo "<script>console.log('$psw'); </script>";
+    $sql="SELECT * FROM renter where EMAIL = :e AND PASSWORD = :p";
+    $stmt=$pdo->prepare($sql);
+      $stmt->execute(array(
+          ':e' => $_POST['email'],
+          ':p' => $psw,
+      ));
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      if($row!==FALSE)
+      {
+        $_SESSION['R_ID']=$row['R_ID'];
+        header("Location: index2.php");
+      }
+      else
+      {
+        $msg="No user exists with this email and password.";
+      }
+  }
+?>
+
 <!DOCTYPE html>
 <html lang = 'en'>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <title>Carbnb - Car Rentals</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Log In</title>
     <link href="login_style.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
      <div id = "div1">
        <div class ='title'>
-         <a href = "./index.html"><h1 class = 'sitehead'>Carbnb</h1></a>
+         <a href = "./index.php"><h1 class = 'sitehead'>Carbnb</h1></a>
        </div>
+       <form action="login.php" method = "POST">
        <div class = 'textbox'>
           <input type = "email" name = "email" class = "email" placeholder="Email">
-          <input type = "password" name = "password" id = "password" placeholder="Password">
-        </div>
-        <input type="submit" name = "submit" value="Sign in">
-        <br><br>
-        <a id = 'create' href = './register.php'>Don't have an account? Create one.</a>
+          <input type = "password" name = "psw" id = "password" placeholder="Password">
+       </div>
+       <button type="submit">Log In</button>
+       </form>
+       <br><br>
+       <a id = 'create' href = './register.php'>Don't have an account? Create one.</a>
+       <p>
+       <?php
+        if ($msg!="")
+        {
+           echo($msg);
+           $msg="";
+        }
+        ?>
+        </p>
      </div>
   </body>
 </html>
-<?php
-session_start();
-error_reporting(0);
-$conn = mysqli_connect('localhost','root','','carbnb');
-if(isset($_POST['submit']))
-{
-$ret=mysqli_query($con,"SELECT * FROM renter WHERE EMAIL ='".$_POST['username']."' and PASSWORD ='".md5($_POST['password'])."'");
-$num=mysqli_fetch_array($ret);
-if($num>0)
-{
-$extra="dashboard.php";
-$_SESSION['login']=$_POST['username'];
-header("location:dashboard.php");
-exit();
-}
-else
-{
-$_SESSION['login']=$_POST['username'];	
-$uip=$_SERVER['REMOTE_ADDR'];
-$status=0;
-$errormsg="Invalid username or password";
-$extra="login.php";
-}
-}
